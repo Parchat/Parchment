c52edb3bbf91e7844695e362230855672224f3c3
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-07-12 15:57:54 -0400

    whoop

--- src/main/kotlin/net/parchat/parchment/Parchment.kt
+++ src/main/kotlin/net/parchat/parchment/Parchment.kt
@@ -1,9 +1,12 @@
 package net.parchat.parchment
 
 import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.events.ReadyEvent
+import net.dv8tion.jda.api.hooks.EventListener
 import net.dv8tion.jda.api.requests.GatewayIntent
 import net.parchat.parchment.misc.Methods
 import net.parchat.parcore.Parcore
+import net.parchat.parcore.listeners.interfaces.EventHandler
 import java.io.File
 
 private val INTENTS = listOf(
@@ -20,14 +23,19 @@ private val INTENTS = listOf(
 
 class Parchment(token: String) : Parcore(token, File("./parchment"), INTENTS) {
 
-    override fun onLoad() {
+    @EventHandler
+    override fun onReady(event: ReadyEvent) {
         logger.info("We've booted.")
+
+        logger.info(jda.guilds.size.toString())
     }
 
+    @EventHandler
     override fun onEnable() {
 
     }
 
+    @EventHandler
     override fun onGuildEnable(guild: Guild) {
 
     }
@@ -36,12 +44,3 @@ class Parchment(token: String) : Parcore(token, File("./parchment"), INTENTS) {
 fun main(args: Array<String>) {
     Parchment(Methods.fetchToken(args).toString())
 }
-
-/*
-fun main(args: Array<String>) {
-    JDABuilder.createDefault(Methods.fetchToken(args), INTENTS)
-        .addEventListeners(
-            ParchmentBase()
-        ).build().awaitReady()
-}
- */
