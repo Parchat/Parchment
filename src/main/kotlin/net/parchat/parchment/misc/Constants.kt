7d719ea5a36e8781c98d930ddc7ec95495dc6ff8
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-07-27 15:44:34 -0400

    Move data folder to a top level function

--- src/main/kotlin/net/parchat/parchment/misc/Constants.kt
+++ src/main/kotlin/net/parchat/parchment/misc/Constants.kt
@@ -4,6 +4,8 @@ import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.Option
 import org.apache.commons.cli.Options
 import java.awt.Color
+import java.nio.file.Path
+import kotlin.io.path.Path
 
 fun fetchToken(args: Array<String>): String? {
     val option = Options().apply { addOption(Option.builder("t").hasArg().argName("token").required().build()) }
@@ -16,4 +18,12 @@ fun String.toColor(): Color {
         Integer.valueOf(substring(3, 5), 16),
         Integer.valueOf(substring(5, 7), 16)
     )
+}
+
+fun getDataFolder(): Path {
+    val dataFolder = Path("./parchment")
+
+    if (!dataFolder.toFile().exists()) dataFolder.toFile().mkdirs()
+
+    return dataFolder
 }
