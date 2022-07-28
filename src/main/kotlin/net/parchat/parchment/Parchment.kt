069e627beddeeed8c5b9aa2ab2f5ec77763e6d76
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-07-28 12:10:30 -0400

    Beep

--- src/main/kotlin/net/parchat/parchment/Parchment.kt
+++ src/main/kotlin/net/parchat/parchment/Parchment.kt
@@ -2,9 +2,13 @@ package net.parchat.parchment
 
 import dev.triumphteam.core.jda.JdaApplication
 import me.mattstudios.config.SettingsManager
+import me.mattstudios.config.SettingsManagerBuilder
+import me.mattstudios.config.annotations.TargetObject
+import me.mattstudios.config.beanmapper.PropertyMapper
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.requests.GatewayIntent
 import net.parchat.parchment.config.Config
+import net.parchat.parchment.config.Settings
 import net.parchat.parchment.misc.fetchToken
 import java.io.File
 
@@ -41,7 +45,7 @@ fun main(args: Array<String>) {
 
     val config = SettingsManager
         .from(File("config.yml"))
-        .configurationData(Config::class.java)
+        .configurationData(Settings::class.java)
         .create()
 
     Parchment(fetchToken(args).toString())
